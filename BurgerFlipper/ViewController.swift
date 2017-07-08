fa129ce8a8ee9b2619d986ef64261a205db8c9e7
Kaan <kaankabalak@gmail.com>
2017-07-08 12:03:39 -0700

    fixed bug where multiple lines were appearing

--- BurgerFlipper/ViewController.swift
+++ BurgerFlipper/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -15,35 +16,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var circle: UIView!
     var semiCircleLayer   = CAShapeLayer()
     
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let center = CGPoint (x: circle.frame.size.width / 2, y: circle.frame.size.height / 2)
-        let circleRadius = circle.frame.size.width / 2
-        let circlePath = UIBezierPath(arcCenter: center, radius: circleRadius, startAngle: CGFloat(Double.pi), endAngle: CGFloat(0), clockwise: true)
-        semiCircleLayer.path = circlePath.cgPath
-        semiCircleLayer.fillColor = UIColor.red.cgColor
-        semiCircleLayer.lineWidth = 8
-        circle.layer.addSublayer(semiCircleLayer)
         
-        let angleInRadians: CGFloat = CGFloat(-1*Double.pi/6)
-        let length: CGFloat = 130
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addLine(to: CGPoint(x: 1, y: 0))
-        path.close()
-        path.apply(.init(rotationAngle: angleInRadians))
-        path.apply(.init(scaleX: length, y: length))
+        // gyroscope
         
+        // interval units are in seconds
+        motionManager.gyroUpdateInterval = 0.1
+        motionManager.deviceMotionUpdateInterval = 0.1
+        // each time gyro updates, either return data or error in callback
+        motionManager.startGyroUpdates(to: OperationQueue.current!) {(data, error) in
+            if let myData = data {
+                if myData.rotationRate.x > 6 {
+                    print("----------Flick!", myData.rotationRate.x, "-----------")
+                }
+            }
+        }
         
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath
-        layer.fillColor = UIColor.blue.cgColor
-        layer.strokeColor = UIColor.blue.cgColor
-        layer.lineWidth = 8
-        line.layer.addSublayer(layer)
-        
-        line.layer.zPosition = 1;
+        // each time orientation changes, either return data or error in callback
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) {(data, error) in
+            if let myData = data {
+                // yaw is initialized to 0 depending on orientation of phone
+                // when app launches, start updating orientation
+                // when user clicks on 'Play Game', save that yaw orientation in some variable init
+                // -90 <= initial_angle <= +90
+                // draw semi circle
+                let center = CGPoint (x: self.circle.frame.size.width / 2, y: self.circle.frame.size.height / 2)
+                let circleRadius = self.circle.frame.size.width / 2
+                let circlePath = UIBezierPath(arcCenter: center, radius: circleRadius, startAngle: CGFloat(Double.pi), endAngle: CGFloat(0), clockwise: true)
+                self.semiCircleLayer.path = circlePath.cgPath
+                self.semiCircleLayer.fillColor = UIColor.red.cgColor
+                self.semiCircleLayer.lineWidth = 8
+                self.circle.layer.addSublayer(self.semiCircleLayer)
+                
+                self.line.layer.sublayers = nil
+                
+                
+                // draw line
+                let angleInRadians: CGFloat = CGFloat(-1*myData.attitude.yaw)
+                let length: CGFloat = 130
+                let path = UIBezierPath()
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: 1, y: 0))
+                path.close()
+                path.apply(.init(rotationAngle: angleInRadians))
+                path.apply(.init(scaleX: length, y: length))
+                
+                let layer = CAShapeLayer()
+                layer.path = path.cgPath
+                layer.fillColor = UIColor.blue.cgColor
+                layer.strokeColor = UIColor.blue.cgColor
+                layer.lineWidth = 8
+                self.line.layer.addSublayer(layer)
+                
+                self.line.layer.zPosition = 1
+                print(self.degrees(myData.attitude.yaw))
+            }
+        }
+    }
+    
+    func degrees(_ radians: Double) -> Double {
+        return 180/Double.pi * radians
     }
 
     override func didReceiveMemoryWarning() {
